6e1857148c81755f0f21033fe46b5aaa37e6e5a0
unknown <alanya879@gmail.com>
2022-01-26 16:21:47 +0300

    user controller up

--- V2/src/controllers/users.js
+++ V2/src/controllers/users.js
@@ -16,10 +16,10 @@ class Users {
         // console.log(req.body.password,cryptedPassword);
         UserService.create(req.body)
             .then((response) => {
-                res.status(httpStatus.CREATED).send(response)
+                res.status(httpStatus.CREATED).json(response)
             })
             .catch((err) => {
-                res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err)
+                res.status(httpStatus.INTERNAL_SERVER_ERROR).json(err)
             })
     }
     login(req, res) {
@@ -29,7 +29,7 @@ class Users {
                 if (!user) {
                     return res
                         .status(httpStatus.NOT_FOUND)
-                        .send({ message: 'User not found' })
+                        .json({ message: 'User not found' })
                 }
                 user = {
                     ...user.toObject(),
@@ -39,50 +39,50 @@ class Users {
                     },
                 }
                 delete user.password
-                res.status(httpStatus.OK).send(user)
+                res.status(httpStatus.OK).json(user)
             })
             .catch((err) => {
-                res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err)
+                res.status(httpStatus.INTERNAL_SERVER_ERROR).json(err)
             })
     }
     noteList(req, res) {
         NoteService.list({ user_id: req.user?._id })
             .then((notes) => {
-                res.status(httpStatus.OK).send(notes)
+                res.status(httpStatus.OK).json(notes)
             })
             .catch((e) => {
-                res.status(httpStatus.INTERNAL_SERVER_ERROR).send({ error: e })
+                res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ error: e })
             })
     }
     index(req, res) {
         console.log(`list`, req.user)
         UserService.list()
             .then((response) => {
-                res.status(httpStatus.CREATED).send(response)
+                res.status(httpStatus.CREATED).json(response)
             })
             .catch((err) => {
-                res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err)
+                res.status(httpStatus.INTERNAL_SERVER_ERROR).json(err)
             })
     }
     deleteUser(req, res) {
         if (!req.params.id) {
             return res
                 .status(httpStatus.BAD_REQUEST)
-                .send({ message: 'ID bilgisi hatalı' })
+                .json({ message: 'ID bilgisi hatalı' })
         } else {
             UserService.delete(req.params?.id)
                 .then((deleteUser) => {
                     if (!deleteUser) {
-                        return res.status(httpStatus.NOT_FOUND).send({
+                        return res.status(httpStatus.NOT_FOUND).json({
                             message: 'Böyle bir kullanıcı bulunmamaktadır',
                         })
                     }
-                    res.status(httpStatus.OK).send({
+                    res.status(httpStatus.OK).json({
                         message: 'Kullanıcı Silinmiştir.',
                     })
                 })
                 .catch((e) =>
-                    res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
+                    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
                         error: 'Silme İşlemi Sırasında Bir Problem Oluştu',
                     })
                 )
@@ -102,34 +102,34 @@ class Users {
 
         if (!req?.files?.profile_image) {
             return res
-                .send(httpStatus.BAD_REQUEST)
-                .send({ error: 'İzin verilmeyen dosya ' })
+                .json(httpStatus.BAD_REQUEST)
+                .json({ error: 'İzin verilmeyen dosya ' })
         }
         req.files.profile_image.mv(folderPath, function (err) {
             if (err) {
                 return res
                     .status(httpStatus.INTERNAL_SERVER_ERROR)
-                    .send({ error: err })
+                    .json({ error: err })
             }
         })
         console.log('resim yüklenmiştir.')
         UserService.update({ _id: req.user._id }, { profile_image: fileName })
             .then((updatedUser) => {
-                res.status(httpStatus.OK).send({ message: updatedUser })
+                res.status(httpStatus.OK).json({ message: updatedUser })
             })
             .catch((e) =>
-                res.status(httpStatus.INTERNAL_SERVER_ERROR).send({ error: e })
+                res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ error: e })
             )
-        //res.status(httpStatus.OK).send({message:"Profil resmi başarılı bir şekilde yüklendi.. "})
+        //res.status(httpStatus.OK).json({message:"Profil resmi başarılı bir şekilde yüklendi.. "})
     }
     changePassword(req, res) {
         req.body.password = passwordToHash(req.body.password)
         UserService.update({ _id: req.user?._id }, req.body)
             .then((updatedUser) => {
-                res.status(httpStatus.OK).send(updatedUser)
+                res.status(httpStatus.OK).json(updatedUser)
             })
             .catch((e) => {
-                res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
+                res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
                     message: e,
                 })
             })
@@ -145,20 +145,20 @@ class Users {
                 if (!updatedUser) {
                     return res
                         .status(httpStatus.NOT_FOUND)
-                        .send({ message: 'Böyle Bir Kullanıcı Bulunamadı.' })
+                        .json({ message: 'Böyle Bir Kullanıcı Bulunamadı.' })
                 }
                 eventEmiter.emit('send_mail', {
                     to: updatedUser.email,
                     subject: 'Şifre Sıfırlama ✔️',
                     html: `Talebiniz üzerine şifreniz sıfırlanmıştır. <br/> Giriş Yaptıktan Sonra Şifrenizi Değiştirmeyi Unutmayınız. <br/> Yeni şifreniz<b> ${newPassword}</b>`,
                 })
-                res.status(httpStatus.OK).send({
+                res.status(httpStatus.OK).json({
                     message:
                         'Şifre Sıfırlama İşlemi İçin Sisteme Kayıtlı Eposta Adresinize Gerekli Bilgileri Gönderdik.',
                 })
             })
             .catch((e) => {
-                res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
+                res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
                     error: e.message,
                 })
             })
@@ -167,10 +167,10 @@ class Users {
     update(req, res) {
         UserService.update({ _id: req.user?._id }, req.body)
             .then((updatedUser) => {
-                res.status(httpStatus.OK).send(updatedUser)
+                res.status(httpStatus.OK).json(updatedUser)
             })
             .catch((e) => {
-                res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
+                res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
                     message: e,
                 })
             })
